0aa1995d5a19fac236e32bf109c4888df2f7db9d
thiago-33 <thiagoalvesdacruz63@gmail.com>
2024-11-05 18:20:31 -0300

    Update MapScreen.js
    alteração

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const MapScreen = () => {
-  return (
+  
+  // Definindo a função fora do JSX
+  const generateMapLink = (coordinates) => {
+    return `https://www.google.com/maps/search/?api=1&query=${coordinates.lat},${coordinates.lng}`;
+  };
 
-    
+  // Exemplo de uso da função generateMapLink
+  const coordinates = { lat: -23.550520, lng: -46.633308 }; // Exemplo de coordenadas
+  const mapLink = generateMapLink(coordinates); // Aqui você pode gerar o link do mapa
+  
+  return (
     <View style={styles.container}>
       <Text style={styles.title}>Mapa de Pontos de Coleta</Text>
       <Text>Carregando mapa...</Text>
+      {/* Exemplo de como exibir o link gerado */}
+      <Text>{mapLink}</Text>
     </View>
   );
 };
